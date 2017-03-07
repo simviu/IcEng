d1987c445008333678ebcb7dc61cdb3986d4a450
Sherman Chen <schen@simviu.com>
2017-03-07 18:17:02 +0800

    New Directory structure.

--- src/demo/pc/main.cpp
+++ src/demo/pc/main.cpp
@@ -3,5 +3,6 @@
 int main(int argc, char *argv[])
 {
     DemoApp app;
+    app.m_cfg.m_sPathRes = "../../../../../IcData/";
     app.runCmdLine(argc, argv);
 }
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -15,5 +15,6 @@
 int main(int argc, char **argv)
 {
     MyTestApp app;
+    app.m_cfg.m_sPathRes = "../../../../../IcData/";
     return app.runCmdLine(argc, argv);
 }
